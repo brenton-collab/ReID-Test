2185701f06a5eaa4d0d11b00f6c1d67de692523c
brenton-collab <brenton@brentonzinck.com>
2026-08-14 13:40:35 -0400

    Bump Relay Capture to v1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ca.brentzinck.relaycapture"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.2.0"
+        versionCode = 31
+        versionName = "1.2.1"
     }
 
     if (hasReleaseSigning) {
