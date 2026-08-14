58830e9960723e6efc8fc2b66ae342fba18d551d
brenton-collab <brenton@brentonzinck.com>
2026-08-14 17:10:00 -0400

    Bump Relay Product UI 2.0 to v1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ca.brentzinck.relaycapture"
         minSdk = 26
         targetSdk = 35
-        versionCode = 38
-        versionName = "1.5.1"
+        versionCode = 39
+        versionName = "1.6.0"
     }
 
     if (hasReleaseSigning) {
